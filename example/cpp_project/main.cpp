afa274a65c1fe5850acff68032d8613e497f8940
João Antônio Cardoso <joao.maker@gmail.com>
2023-05-12 15:51:57 -0300

    example: cpp_project: Mirror the Python example

--- example/cpp_project/main.cpp
+++ example/cpp_project/main.cpp
@@ -3,6 +3,12 @@
 #include "bindings.h"
 
 int main() {
-    printf("Tire width: %f\n", create_random_tire().size.width);
+    auto tire = create_random_tire();
+    const char *materials[] = { "Plastic", "Rubber" };
+
+    printf("Tire pressure: %f\n", tire.pressure);
+    printf("Tire material: %s\n", materials[(unsigned int)tire.material]);
+    printf("Tire size: %fw, %fh\n", tire.size.width, tire.size.height);
+
     return 0;
 }
