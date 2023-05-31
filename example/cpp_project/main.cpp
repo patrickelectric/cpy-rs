345b52cc1d3b6a4bd110add012eb4fc1692b132a
Raul Victor Trombin <raulvtrombin@gmail.com>
2023-05-31 15:39:52 -0300

    example: cpp: Format the code

--- example/cpp_project/main.cpp
+++ example/cpp_project/main.cpp
@@ -6,9 +6,9 @@ int main() {
     auto tire = create_random_tire();
     const char *materials[] = { "Plastic", "Rubber" };
 
-    printf("Tire pressure: %f\n", tire.pressure);
+    printf("Tire pressure: %.2f\n", tire.pressure);
     printf("Tire material: %s\n", materials[(unsigned int)tire.material]);
-    printf("Tire size: %fw, %fh\n", tire.size.width, tire.size.height);
+    printf("Tire size: %.2fw, %.2fh\n", tire.size.width, tire.size.height);
     printf("The tire have an aspect ratio of %.2f\n",wheel_size_aspect(tire.size.width, tire.size.height));
 
     func_with_no_return();
