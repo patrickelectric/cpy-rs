e5de5cb883e47014c9bd04396392a53b994381da
Raul Victor Trombin <raulvtrombin@gmail.com>
2023-05-31 15:36:00 -0300

    example: python: Format the code

--- example/py_project/main.py
+++ example/py_project/main.py
@@ -1,9 +1,8 @@
 import example
 
 tire = example.create_random_tire()
-print(f'Tire pressure: {tire.pressure}')
+print(f'Tire pressure: {tire.pressure:.2f}')
 print(f'Tire material: {tire.material}')
-print(f'Tire size: {tire.size.width}w, {tire.size.height}h')
 print(f'Tire size: {tire.size.width:.2f} width, {tire.size.height:.2f} height')
 
 example.func_with_no_return()
