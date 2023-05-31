9b9f071210a08cca47c863ca7a419e056ab7c4ff
Raul Victor Trombin <raulvtrombin@gmail.com>
2023-05-31 15:36:24 -0300

    example: python: Add function using inputs

--- example/py_project/main.py
+++ example/py_project/main.py
@@ -4,5 +4,6 @@ tire = example.create_random_tire()
 print(f'Tire pressure: {tire.pressure}')
 print(f'Tire material: {tire.material}')
 print(f'Tire size: {tire.size.width}w, {tire.size.height}h')
+print(f'Tire size: {tire.size.width:.2f} width, {tire.size.height:.2f} height')
 
 example.func_with_no_return()
