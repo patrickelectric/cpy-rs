4dd12b6c33b207d77a1127e0204fb2f857e0dab6
Raul Victor Trombin <raulvtrombin@gmail.com>
2023-05-31 12:23:02 -0300

    src: Rename structures methods

--- src/lib.rs
+++ src/lib.rs
@@ -12,17 +12,19 @@ macro_rules! export_cpy {
     };
     (@process_item) => {};
     (@process_item enum $name:ident { $($variant:ident,)* } $($rest:tt)*) => {
+        export_cpy!(@generate_enum $name { $($variant,)* });
         export_cpy!(@process_item $($rest)*);
     };
     (@process_item struct $name:ident { $($field:ident : $ftype:ty,)* } $($rest:tt)*) => {
-        export_cpy!(@struct $name { $($field : $ftype,)* });
+        export_cpy!(@generate_struct $name { $($field : $ftype,)* });
         export_cpy!(@process_item $($rest)*);
     };
     (@process_item fn $name:ident() $(-> $ret:ty)? $body:block $($rest:tt)*) => {
-        export_cpy!(@fn $name() $(-> $ret)? $body);
+        export_cpy!(@generate_function $name() $(-> $ret)? $body);
         export_cpy!(@process_item $($rest)*);
     };
 
+    (@generate_enum $name:ident { $($variant:ident,)* }) => {
         #[derive(Clone, Debug)]
         #[repr(C)]
         #[cfg_attr(feature = "python", pyo3::prelude::pyclass)]
@@ -32,7 +34,7 @@ macro_rules! export_cpy {
             )*
         }
     };
-    (@struct $name:ident { $($field:ident : $ftype:ty,)* }) => {
+    (@generate_struct $name:ident { $($field:ident : $ftype:ty,)* }) => {
         #[derive(Clone, Debug)]
         #[repr(C)]
         #[cfg_attr(feature = "python", pyo3::prelude::pyclass(get_all, set_all))]
@@ -42,7 +44,7 @@ macro_rules! export_cpy {
             )*
         }
     };
-    (@fn $name:ident() $(-> $ret:ty)? $body:block) => {
+    (@generate_function $name:ident() $(-> $ret:ty)? $body:block) => {
         #[no_mangle]
         #[cfg_attr(feature = "python", pyfunction)]
         pub extern "C" fn $name() $(-> $ret)? {
