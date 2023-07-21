8c93d3c4feaf8ac951b5d463858bbf4565cc1921
Patrick José Pereira <patrickelectric@gmail.com>
2023-07-21 17:28:00 -0300

    example: Add format_size_of_wheels for dynamic size arrays
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- example/cpp_project/main.cpp
+++ example/cpp_project/main.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <vector>
 #include "bindings.h"
 
 int main() {
@@ -10,8 +11,11 @@ int main() {
     printf("Tire material: %s\n", materials[(unsigned int)tire.material]);
     printf("Tire size: %.2fw, %.2fh\n", tire.size.width, tire.size.height);
     printf("The tire have an aspect ratio of %.2f\n",wheel_size_aspect(tire.size.width, tire.size.height));
-    uint8_t values[3] = {77, 42, 69};
-    format_wheel_identifier(&values);
+    std::vector<uint8_t> values = {77, 42, 69};
+    format_wheel_identifier(reinterpret_cast<const uint8_t (*)[3]>(&values[0]));
+    format_size_of_wheels(reinterpret_cast<const uint8_t*>(&values[0]), values.size());
+    values.push_back(30);
+    format_size_of_wheels(reinterpret_cast<const uint8_t*>(&values[0]), values.size());
 
     func_with_no_return();
 
