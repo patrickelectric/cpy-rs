65cafaf5cd09042e593b67c95487b00fbecc2463
Patrick José Pereira <patrickelectric@gmail.com>
2023-07-21 14:49:30 -0300

    examples: Add call for format_wheel_identifier
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- example/py_project/main.py
+++ example/py_project/main.py
@@ -4,5 +4,5 @@ tire = example.create_random_tire()
 print(f'Tire pressure: {tire.pressure:.2f}')
 print(f'Tire material: {tire.material}')
 print(f'Tire size: {tire.size.width:.2f} width, {tire.size.height:.2f} height')
-
+example.format_wheel_identifier([77, 42, 69])
 example.func_with_no_return()
